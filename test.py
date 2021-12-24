8873ea58501e9ea470a0596284ca43536c9f8adc
YFender <yurik2159@inbox.ru>
2021-12-24 21:56:39 +0300

    л

--- test.py
+++ test.py
@@ -1,23 +1,49 @@
-import json
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-data = {}
-data['people'] = []
-data['people'].append({
-    'name': 'Scott',
-    'website': 'pythonist.ru',
-    'from': 'Nebraska'
-})
-data['people'].append({
-    'name': 'Larry',
-    'website': 'pythonist.ru',
-    'from': 'Michigan'
-})
-data['people'].append({
-    'name': 'Tim',
-    'website': 'pythonist.ru',
-    'from': 'Alabama'
-})
+class Demo(QtWidgets.QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+        self.button = QtWidgets.QPushButton()
+        self.label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignCenter)
 
-with open('data.txt', 'a') as outfile:
-    json.dump(data, outfile)
-    outfile.write("\n")
+        self.combo = QtWidgets.QComboBox(self)
+        self.combo.currentIndexChanged.connect(self.change_func)
+
+        self.trans = QtCore.QTranslator(self)
+
+        self.v_layout = QtWidgets.QVBoxLayout(self)
+        self.v_layout.addWidget(self.combo)
+        self.v_layout.addWidget(self.button)
+        self.v_layout.addWidget(self.label)
+
+        options = ([('English', ''), ('français', 'eng-fr' ), ('中文', 'eng-chs'), ])
+
+        for i, (text, lang) in enumerate(options):
+            self.combo.addItem(text)
+            self.combo.setItemData(i, lang)
+        self.retranslateUi()
+
+    @QtCore.pyqtSlot(int)
+    def change_func(self, index):
+        data = self.combo.itemData(index)
+        if data:
+            self.trans.load(data)
+            QtWidgets.QApplication.instance().installTranslator(self.trans)
+        else:
+            QtWidgets.QApplication.instance().removeTranslator(self.trans)
+
+    def changeEvent(self, event):
+        if event.type() == QtCore.QEvent.LanguageChange:
+            self.retranslateUi()
+        super(Demo, self).changeEvent(event)
+
+    def retranslateUi(self):
+        self.button.setText(QtWidgets.QApplication.translate('Demo', 'Start'))
+        self.label.setText(QtWidgets.QApplication.translate('Demo', 'Hello, World'))
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    demo = Demo()
+    demo.show()
+    sys.exit(app.exec_())
